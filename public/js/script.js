013ef6d7a44c55b1e8f74df7ea68e8c42fc3e0cf
tiffylou <tiffany6872@gmail.com>
2014-04-12 13:45:26 -0400

    script updates

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,13 @@
 $(document).ready(function(){
-
+    
+    var position = null;
+    $(function(){
+        if (navigator.geolocation)
+        {
+            navigator.geolocation.getCurrentPosition(function(pos) {
+                position = [pos.coords.latitude, pos.coords.longitude];
+            });
+        }    
+    });
 
 });
