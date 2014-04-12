f13f07dff4582f2d64332cd4415e390439885b94
Anton Pantev <pantevanton@gmail.com>
2014-04-12 11:49:33 -0400

    Made the server randomly return a result for each category and each category is returned in a random order

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.set('json spaces', 2);
 
+var categories = ["arts,active", "nightlife", "restaurants"];
+
 app.get('/yelp', function(req, res) {
     var params;
     
@@ -25,13 +27,74 @@ app.get('/yelp', function(req, res) {
         params = {location: "Montreal"};
     }
     
-    params.sort = 2;
-    params.category_filter = "restaurants";
+    var count = categories.length;
+    var arr = [];
     
-    yelp.search(params, function(error, data) {
-        res.json(data);
-    });
+    for(var i in categories) {
+        var apiParams = createYelpParams(params);
+        apiParams.category_filter = categories[i];
+        yelp.search(apiParams, createYelpCallback(categories[i]));
+    }
+    
+    function createYelpCallback(category) {
+
+        return function(error, data) {
+            data.businesses.sort(function(a,b) {
+                if(a.distance < b.distance) {
+                    return -1;
+                } else if (a.distance == b.distance) {
+                    return 0;
+                } else {
+                    return 1;
+                }
+            });
+            
+            arr.push({category: category, result: data.businesses});
+            count--;
+            
+            /* If we are done sending requests to yelp then return the result */
+            if(count == 0) {
+                shuffle(arr);
+                for(var i in arr) {
+                    var result = arr[i].result[Math.floor(Math.random()*arr[i].result.length)];
+                    arr[i].result = result;
+                }
+                res.json(arr);
+            }
+        };
+    }
 });
+
+function createYelpParams(params) {
+    var ret = {sort: 2, radius_filter:40000};
+    
+    for(var i in params) {
+        ret[i] = params[i];
+    }
+    return ret;
+}
+
+function shuffle(array) {
+  var currentIndex = array.length
+    , temporaryValue
+    , randomIndex
+    ;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
