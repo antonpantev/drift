3ca1d68bcecc174ffceaa18d3e4b506b6c934f13
Anton Pantev <pantevanton@gmail.com>
2014-04-12 16:32:08 -0400

    Initializing the map on the results page.

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,5 +1,20 @@
 var position = null;
 
+var directionsDisplay;
+var directionsService = new google.maps.DirectionsService();
+var map;
+
+function initializeMap() {
+    directionsDisplay = new google.maps.DirectionsRenderer();
+    var chicago = new google.maps.LatLng(position[0], position[1]);
+    var mapOptions = {
+        zoom: 6,
+        center: chicago
+    }
+    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    directionsDisplay.setMap(map);
+}
+
 $(document).ready(function(){
     $("#loading").hide();
     $("#readyButton").prop("disabled",true);
@@ -20,7 +35,9 @@ $(document).ready(function(){
         $.get('/yelp', params, function(data) {
             $("#loading").hide();
             $("#page1").hide('slow');
-            $("#page2").show('slow');
+            $("#page2").show('slow', function() {
+                initializeMap();
+            });
             console.log(data);
         });
     });
